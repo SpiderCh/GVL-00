04da5991a33dc7229b6d34f4f74c23dad1f19779
CHAS.CH <spider.ch@gmail.com>
2016-08-01 00:11:40 +0700

    clone test run.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,57 @@
 #include <iostream>
+#include <unistd.h>
 
 #include "git++/GitCore.hpp"
+#include "git++/GitRepository.hpp"
+
+
+//unsigned int	total_objects
+//unsigned int	indexed_objects
+//unsigned int	received_objects
+//unsigned int	local_objects
+//unsigned int	total_deltas
+//unsigned int	indexed_deltas
+//size_t	received_bytes
 
 int main(int argc, char** argv)
 {
 	(void) argc;
 	(void) argv;
-	git::InitGitCore();
-	std::cout << "Everything Ok." << std::endl;
-	git::DeInitGitCore();
+	Git::Repository::CloneCallbacks cb;
+	cb.fetchProgress = [](unsigned int	total_objects
+						, unsigned int	indexed_objects
+						, unsigned int	received_objects
+						, unsigned int	local_objects
+						, unsigned int	total_deltas
+						, unsigned int	indexed_deltas
+						, size_t	received_bytes)
+	{
+		std::cout << "Fetch progress:"       << std::endl
+				  << "\tTotal objects:\t"    << total_objects    << std::endl
+				  << "\tIndexed objects:\t"  << indexed_objects  << std::endl
+				  << "\tReceived objects:\t" << received_objects << std::endl
+				  << "\tLocal objects:\t"    << local_objects    << std::endl
+				  << "\tTotal deltas:\t"     << total_deltas     << std::endl
+				  << "\tIndexed deltas:\t"   << indexed_deltas   << std::endl
+				  << "\tRecived bytes:\t"    << received_bytes   << std::endl;
+	};
+	cb.checkoutProgress = [](const char*, size_t current, size_t total)
+	{
+		std::cout << "Checkout progress for: " << /*path << " :" <<*/ std::endl
+				  << "\tCurrent:\t" << current << std::endl
+				  << "\tTotal:\t"   << total   << std::endl;
+	};
+
+	Git::InitGitCore();
+	Git::Repository repo;
+	const bool res = repo.Clone("/home/chas/Projects/git/test/gvl-01", "https://github.com/SpiderCh/GVL-00.git", cb);
+	if(res) {
+		std::cout << "Successfully clone repository";
+	} else {
+		Git::GitError err = Git::GetLastError();
+		std::cout << "Cannot clone repository" << std::endl
+				  << "\tMessage: " << err.message << std::endl
+				  << "\tError Code: " << err.errorType << std::endl;
+	}
 	return 0;
 }
